c739256b1ee13f949032aa4b26a50ae417237bdc
itiro-y <itiro_yoshii@hotmail.com>
2022-05-04 02:28:34 -0300

    important_links, updt README.md

---
+++ LearningPython/range_func/range.py
@@ -0,0 +1,13 @@
+# The range() function
+# When used in a for loop the range() function can be treated as a built-in function
+# that returns an iterable object
+
+# With one argument -> counts from 0 to stop
+# range(stop)
+
+# With two arguments -> counts from start to stop
+# range(start, stop)
+
+# With three arguments -> it counts from start to stop, with a defined step
+# range(start, stop, step)
+
